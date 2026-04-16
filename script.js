022672f2600f6df0cd546d419b787c5e8a05ad56
charanudari17 <charanudari835@gmail.com>
2026-04-17 01:04:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+
+}
