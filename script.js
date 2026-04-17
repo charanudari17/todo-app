f3117290b70d7c33c7342873578a65711d4472a5
charanudari17 <charanudari835@gmail.com>
2026-04-17 09:50:11 +0530

    added delete All features

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 function addTask() {
     let input = document.getElementById("taskInput");
     let task = input.value;
@@ -7,9 +6,34 @@ function addTask() {
 
     let li = document.createElement("li");
     li.textContent = task;
+li.onclick = function () {
+    li.style.textDecoration = "line-through";
+};
+    // Create delete button
+    let btn = document.createElement("button");
+    btn.textContent = "Remove Task";
+
+    // When button clicked → remove task
+    btn.onclick = function (event) {
+    event.stopPropagation(); // prevents li click
+    li.remove();
+};
+
+    li.appendChild(btn);
 
     document.getElementById("taskList").appendChild(li);
 
     input.value = "";
+}
 
+// 👉 THIS SHOULD BE OUTSIDE
+function deleteAll() {
+    let list = document.getElementById("taskList");
+
+    if (list.children.length === 0) {
+        alert("No tasks to delete!");
+        return;
+    }
+
+    list.innerHTML = "";
 }
